880bed6cb79d46764e1f623b6bea16061a17ab2f
Andres Sanchez <a.sanchez2001@uniandes.edu.co>
2025-03-01 01:44:08 +0000

    Cambiar string de conexion

--- src/saludtechalpes/api/__init__.py
+++ src/saludtechalpes/api/__init__.py
@@ -25,9 +25,8 @@ def create_app(configuracion=None):
     global app
     # Init la aplicacion de Flask
     app = Flask(__name__, instance_relative_config=True)
-
-    # Configuracion de BD
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/saludtechalpes'
+    app.config['SQLALCHEMY_DATABASE_URI'] =(
+            f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
      # Inicializa la DB

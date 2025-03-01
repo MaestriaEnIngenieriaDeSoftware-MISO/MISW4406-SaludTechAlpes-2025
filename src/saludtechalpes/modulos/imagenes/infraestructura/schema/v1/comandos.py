e25a0fad0ac1fa37b02668882942b590923f4af3
Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
2025-03-01 02:03:22 +0000

    fixing ports

--- src/saludtechalpes/modulos/imagenes/infraestructura/schema/v1/comandos.py
+++ src/saludtechalpes/modulos/imagenes/infraestructura/schema/v1/comandos.py
@@ -3,7 +3,8 @@ from dataclasses import dataclass, field
 from saludtechalpes.seedwork.infraestructura.schema.v1.comandos import ComandoIntegracion
 
 class ComandoObtenerImagenPayload(ComandoIntegracion):
-    id: String()
+    tipo_imagen = String()
+    tipo_patologia = String()
 
 class ComandoObtenerImagen(ComandoIntegracion):
     data = ComandoObtenerImagenPayload()

193bd7bcdc66c7701c5e9405a01a357d2c0036b2
Edgar Melara <e.melara@uniandes.edu.co>
2025-03-02 21:33:36 +0000

    feat: enviar 202 si se conecta con el servicio de exportaciones

--- src/bff_sta/api/v1/esquemas.py
+++ src/bff_sta/api/v1/esquemas.py
@@ -8,26 +8,13 @@ from datetime import datetime
 
 
 EXPORTACION_STA_HOST = os.getenv("EXPORTACION_STA_ADDRESS", default="exportacionsta")
-FORMATO_FECHA = '%Y-%m-%dT%H:%M:%SZ'
 
-def obtener_imagenes(root) -> typing.List["Imagen"]:
-    imagenes_json = requests.get(f'http://{EXPORTACION_STA_HOST}:5000/exportar').json()
-    imagenes = []
+def obtener_imagenes(root) -> int:
+    response = requests.get(f'http://{EXPORTACION_STA_HOST}:5001/exportar')
+    
+    if response.status_code in [200, 202]:
+        return 202
+    else:
+        response.raise_for_status()
 
-    for reserva in imagenes_json:
-        imagenes.append(
-            Imagen(
-                tipo_patologia=reserva['tipo_patologia'],
-                tipo_imagen=reserva['tipo_imagen']
-            )
-        )
-
-    return imagenes
-
-
-@strawberry.type
-class Imagen:
-    tipo_patologia: str
-    tipo_imagen : str 
-   
     

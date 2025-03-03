09732094d4e80c7885891ad3440bd34a14f08174
Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
2025-03-03 00:03:55 +0000

    bff working with docker

--- src/bff_sta/api/v1/esquemas.py
+++ src/bff_sta/api/v1/esquemas.py
@@ -8,9 +8,10 @@ from datetime import datetime
 
 
 EXPORTACION_STA_HOST = os.getenv("EXPORTACION_STA_ADDRESS", default="localhost")
+EXPORTACION_STA_PORT = os.getenv("EXPORTACION_STA_PORT", default="5000")
 
 def obtener_imagenes(root, tipo_patologia: str, tipo_imagen: str):
-    url = f'http://{EXPORTACION_STA_HOST}:5001/exportar'
+    url = f'http://{EXPORTACION_STA_HOST}:{EXPORTACION_STA_PORT}/exportar'
     headers = {'Content-Type': 'application/json'}
     data = {
         "tipo_patologia": tipo_patologia,
@@ -19,7 +20,7 @@ def obtener_imagenes(root, tipo_patologia: str, tipo_imagen: str):
     response = requests.post(url, headers=headers, json=data)
     
     if response.status_code in [200, 202]:
-        return 202
+        return StatusResponse(status_code=response.status_code)
     else:
         response.raise_for_status()
 

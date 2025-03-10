63c8916bc4613c87b293dba15456118486994d1a
edgar-melara-uniandes <e.melara@uniandes.edu.co>
2025-03-10 09:43:00 -0600

    fix: arreglar problema suscribiendo al topico (#7)

---
+++ src/bff_sta/eventos.py
@@ -0,0 +1,25 @@
+from pulsar.schema import *
+import uuid
+import time
+
+
+def time_millis():
+    return int(time.time() * 1000)
+
+class EventoIntegracion(Record):
+    id = String(default=str(uuid.uuid4()))
+    time = Long()
+    ingestion = Long(default=time_millis())
+    specversion = String()
+    type = String()
+    datacontenttype = String()
+    service_name = String()
+
+class EventoExportacionImagenesFinalizadoPayload(Record):
+    mensaje = String()
+    cantidad_imagenes_exportadas = Integer()
+    estado = String()
+    timestamp = Long()
+
+class NotificacionEvento(EventoIntegracion):
+    data = EventoExportacionImagenesFinalizadoPayload()
